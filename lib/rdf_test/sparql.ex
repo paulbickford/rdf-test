00c2b9308e6c53081a751e384de480025cefa7f8
Paul Bickford <20016990+paulbickford@users.noreply.github.com>
2020-05-19 22:54:28 +0000

    Create query pages.

--- lib/rdf_test/sparql.ex
+++ lib/rdf_test/sparql.ex
@@ -9,4 +9,28 @@ defmodule RdfTest.Sparql do
   def list_queries do
     Repo.all(Query)
   end
+
+  def change_query(%Query{} = query) do
+    Query.changeset(query, %{})
+  end
+
+  def create_query(attrs \\ %{}) do
+    %Query{}
+    |> Query.changeset(attrs)
+    |> Repo.insert()
+  end
+
+  def get_query!(id) do
+    Repo.get!(Query, id)
+  end
+
+  def update_query(%Query{} = query, attrs) do
+    query
+    |> Query.changeset(attrs)
+    |> Repo.update()
+  end
+
+  def delete_query(%Query{} = query) do
+    Repo.delete(query)
+  end
 end
